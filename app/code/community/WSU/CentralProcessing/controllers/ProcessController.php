6fcff0a52b1722d492cc23601f064afee91ef177
jeremyBass <jeremybass@cableone.net>
2014-06-25 12:36:15 -0700

    adds in the basic response handler

--- app/code/community/WSU/CentralProcessing/controllers/ProcessController.php
+++ app/code/community/WSU/CentralProcessing/controllers/ProcessController.php
@@ -157,6 +157,64 @@ class Wsu_CentralProcessing_ProcessController extends Mage_Core_Controller_Front
 
     public function routerAction() {
 		
+		
+		
+
+		$helper				= Mage::helper('centralprocessing');
+		$GUID=$_REQUEST['GUID'];
+		//url-ify the data for the POST
+		$fields_string="RequestGUID=".$GUID;
+		
+		
+		$url = trim($helper->getCentralProcessingUrl(),'/');
+		$url .= DS.($helper->getAuthorizationType()=="AUTHCAP"?"AuthCapResponse":"AuthCapResponse");		
+		
+		
+		$wrapper = fopen('php://temp', 'r+');
+		
+		//open connection
+		$ch = curl_init($url);
+		
+		curl_setopt($ch, CURLOPT_VERBOSE, true);
+		curl_setopt($ch, CURLOPT_STDERR, $wrapper);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		//set the url, number of POST vars, POST data
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_POST, count(1));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+		
+		//execute post
+		$result = curl_exec($ch);
+		if($result === false) {
+			echo 'Curl error: ' . curl_error($ch);
+		}
+		
+		
+		//close connection
+		curl_close($ch);
+		var_dump($url);
+		var_dump($fields_string);
+		
+
+		var_dump($result);
+		
+		$nodes = new SimpleXMLElement($helper->removeResponseXMLNS($result));
+		
+		$ResponseReturnCode = $nodes->ResponseReturnCode;
+		$ResponseGUID = $nodes->ResponseGUID;
+		$ApprovalCode = $nodes->ApprovalCode;
+		$CreditCardType = $nodes->CreditCardType;
+		$MaskedCreditCardNumber = $nodes->MaskedCreditCardNumber;
+		
+		var_dump($ResponseReturnCode);
+		var_dump($ResponseGUID);
+		var_dump($ApprovalCode);
+		var_dump($CreditCardType);
+		var_dump($MaskedCreditCardNumber);
+
+		die();
+		
 	}
 
 
