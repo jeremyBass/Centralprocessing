bdd28a7fb6d6b4cfc2275b821ce323a47a8b3be1
jeremyBass <jeremybass@cableone.net>
2014-06-26 12:50:29 -0700

    typo

--- app/code/community/WSU/CentralProcessing/Model/System/Config/Source/Cards/Type.php
+++ app/code/community/WSU/CentralProcessing/Model/System/Config/Source/Cards/Type.php
@@ -3,7 +3,7 @@
  * @category   Cybersource
  * @package    Wsu_CentralProcessing
  */
-class Wsu_CentralProcessing_Model_System_Config_Source_Card_Type {
+class Wsu_CentralProcessing_Model_System_Config_Source_Cards_Type {
     public function toOptionArray() {
         return array(
             '001'    => Mage::helper('centralprocessing')->__('Visa'),
