621e08a215e18a53b58b5e4f701ad9bfedbafb60
jeremyBass <jeremybass@cableone.net>
2014-06-26 10:51:36 -0700

    there is no need for the template to be set

--- app/code/community/WSU/CentralProcessing/Block/Info.php
+++ app/code/community/WSU/CentralProcessing/Block/Info.php
@@ -6,7 +6,7 @@
 class Wsu_CentralProcessing_Block_Info extends Mage_Payment_Block_Info {
     protected function _construct() {
         parent::_construct();
-        $this->setTemplate('wsu/centralprocessing/info.phtml');
+        //$this->setTemplate('wsu/centralprocessing/info.phtml');
     }
 
     public function getMethodCode() {
