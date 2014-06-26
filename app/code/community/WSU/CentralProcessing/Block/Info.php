b5a1160dd7c8766ecfe61994b4397b3f7771e1ab
jeremyBass <jeremybass@cableone.net>
2014-06-26 15:41:03 -0700

    break out each option so we can later add conditions

--- app/code/community/WSU/CentralProcessing/Block/Info.php
+++ app/code/community/WSU/CentralProcessing/Block/Info.php
@@ -18,18 +18,22 @@ class Wsu_CentralProcessing_Block_Info extends Mage_Payment_Block_Info {
         if ($this->_paymentSpecificInformation !== null) {
             return $this->_paymentSpecificInformation;
         }
-
+		
         $info = $this->getInfo();
 
         $transport = new Varien_Object();
         $transport = parent::_prepareSpecificInformation($transport);
-        $transport->addData(array(
-            Mage::helper('payment')->__('Response Return Code') => "".$info->getResponseReturnCode(),
-            Mage::helper('payment')->__('GUID') => $info->getResponseGuid(),
-			Mage::helper('payment')->__('Approval Code') => $info->getApprovalCode(),
-			Mage::helper('payment')->__('Card Type') => $helper->getCardType($info->getCardType()),
-			Mage::helper('payment')->__('Masked CC Number') => '############'.$info->getMaskedCcNumber()
-        ));
+		
+		$transData=array();
+
+		$transData[Mage::helper('payment')->__('Card Type')]=$helper->getCardType($info->getCardType());
+		$transData[Mage::helper('payment')->__('Masked CC Number')]='############'.$info->getMaskedCcNumber();
+		
+		$transData[Mage::helper('payment')->__('Response Return Code')]="".$info->getResponseReturnCode();
+		$transData[Mage::helper('payment')->__('GUID')]=$info->getResponseGuid();
+		$transData[Mage::helper('payment')->__('Approval Code')]=$info->getApprovalCode();
+
+        $transport->addData($transData);
 
         return $transport;
     }
