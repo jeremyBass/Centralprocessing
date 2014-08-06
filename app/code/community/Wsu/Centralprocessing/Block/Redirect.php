105f460d162a0ed43a904d9a89edbec0e08576f9
Jeremy Bass <jeremybass@cableone.net>
2014-08-06 13:44:25 -0700

    put up a log for bad redirects

--- app/code/community/Wsu/Centralprocessing/Block/Redirect.php
+++ app/code/community/Wsu/Centralprocessing/Block/Redirect.php
@@ -65,14 +65,17 @@ class Wsu_Centralprocessing_Block_Redirect extends Mage_Core_Block_Abstract {
 		var_dump($url);
 		var_dump($fields_string);
 		
-
+		ob_start();
 		var_dump($result);
-		
+		$log = ob_get_clean();
 		$nodes = new SimpleXMLElement($helper->removeResponseXMLNS($result));
 		
 		$urlRedirect = $nodes->WebPageURLAndGUID;
 		
+		ob_start();
 		var_dump($urlRedirect);
+		$log .= ob_get_clean();		
+		file_put_contents("redirect.txt", $log);
 		
 		header("Location: ".$urlRedirect);
 		exit();
